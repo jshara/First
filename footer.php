529acd94aabd1334f3ae0b698cc7ededae65cd63
Joseph <jshara420@gmail.com>
2018-04-23 11:46:39 +1200

    new changes

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 </body>
-<?php //This is addition of comments to this trying file for version 2.1 in lqterfre3fr3?>
+<?php //This is addition of comments to this trying file for version 2.1 ok i am trying again?>
 </html>
